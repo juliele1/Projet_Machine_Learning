b6f072fed296437e960b8919bd919cbd46284caa
juliele1 <julie.chen@efrei.net>
2023-05-02 14:45:16 +0200

    csv pas touché

--- Data_preparation.py
+++ Data_preparation.py
@@ -5,10 +5,25 @@ data_x = pd.read_csv('Data_X.csv')
 data_y = pd.read_csv('Data_Y.csv')
 data_new_x = pd.read_csv('DataNew_X.csv')
 
+"""
 # Avant suppression des lignes ayant des valeurs nulles
 print(data_x)
+print(data_new_x)
 # Recherche des valeurs nulles
 print(data_x.isnull())
+print(data_new_x.isnull())
 # Suppression des lignes ayant des valeurs nulles
 data_x.dropna(inplace=True)
+data_new_x.dropna(inplace=True)
 print(data_x)
+print(data_new_x)
+
+"""
+
+# On remplace les valeurs manquantes par des 0.0
+
+#data_x = data_x.fillna(0.0)
+print(data_x.isnull())
+print(data_x.loc[:,'DE_RAIN'])
+df.loc[:,'SalePrice']
+
